2d8d198321006b9f199ade523e1074735f849f6d
Saimadhav-333 <saimadhav9235@gmail.com>
2024-01-06 14:20:34 +0530

    changes made to result

--- frontend/src/Results.jsx
+++ frontend/src/Results.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const Results = () => {
     const [result, setResult] = useState([]);
+    var p,q;
     const [userdata, setuserdata] = useState({
         name: "",
         section: "",
@@ -12,60 +13,85 @@ const Results = () => {
         rollno: "",
         phoneno: "",
       });
+      const [error, setError] = useState(null);
     useEffect(() => {
         // Your fetch code here to get the user data
         const fetchResult = async () => {
           try {
             const response = await axios.get('http://localhost:2000/api/result/all-result');
             setResult(response.data);
-            console.log(setResult)
+            q=response.data;
+            // console.log(setResult)
+            console.log(response.data)
+
+            return q;
           } catch (error) {
             console.error('Error fetching user result:', error);
           }
         };
     
-        fetchResult();
-
-
+        
+        
         const fetchStudent = async () => {
             try {
-              const accessToken = localStorage.getItem("accessToken");
+                const accessToken = localStorage.getItem("accessToken");
               console.log("Token : ", accessToken);
               if (!accessToken) {
                 setError("Access token not found"); // Handle missing token
                 return;
-              }
-              const response = await axios.get(
+            }
+            const response = await axios.get(
                 "http://localhost:2000/api/users/student-profile",
                 {
-                  headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": "application/json",
-                  },
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                        "Content-Type": "application/json",
+                    },
                 }
-              );
-              if (response && response.data) {
-                p = response.data;
-                setuserdata({
-                  name: response.data.name,
-                  rollno: response.data.rollno,
-                  phoneno: response.data.phoneno,
-                  section: response.data.section,
-                  email: response.data.email,
-                  password: response.data.password,
-                });
-                console.log(response.data);
-                
-              }
+                );
+                if (response && response.data) {
+                    p = response.data;
+                    setuserdata({
+                        name: response.data.name,
+                        rollno: response.data.rollno,
+                        phoneno: response.data.phoneno,
+                        section: response.data.section,
+                        email: response.data.email,
+                        password: response.data.password,
+                    });
+                    console.log(response.data);
+                    
+                    
+                }
+            return p;
             } catch (error) {
-              console.log(error);
-              setError(
-                error.message || "An error occurred while fetching user data."
-              );
-            }
-          };
-      
-          fetchStudent();
+                console.log(error);
+                setError(
+                    error.message || "An error occurred while fetching user data."
+                    );
+                }
+            };
+        const userresult = async ()=>{
+            const user= await fetchStudent();
+            const result= await fetchResult();
+            let userid = user.id
+            console.log(userid);
+            const resultOfUser = result.find(item => item.user === userid);
+            console.log("resultofuser=",resultOfUser);
+            const useroptions=resultOfUser.options
+            console.log(useroptions)
+            console.log(useroptions.length)
+            const useroptionslast = useroptions[useroptions.length -1]
+            console.log(useroptionslast);
+            const Depression = useroptionslast['Depression']
+            const Anxiety = useroptionslast['Anxiety']
+            const Stress = useroptionslast['Stress']
+            console.log(Depression,Anxiety,
+                Stress)
+            // console.log(resultOfUser.options[-1])
+            
+        }
+        userresult();
       }, []);
 
 
