972dfa3c0cc73ca294c85ec432ff4c9fdb8e0eaf
Saimadhav-333 <saimadhav9235@gmail.com>
2024-01-23 22:38:54 +0530

    added teachers dashboard suggestions to the backend

--- backend/backend/controllers/teacherController.js
+++ backend/backend/controllers/teacherController.js
@@ -1,5 +1,6 @@
 const Teacher = require('../model/teacherModel')
 const User = require('../model/userModel')
+const Suggestion = require('../model/suggestionModel')
 const asyncHandler = require('express-async-handler')
 //login for a teacher with given username and password
 const teacherlogin = asyncHandler( async (req,res)=>{
@@ -23,4 +24,35 @@ const studentData = async (req,res)=>{
 
 }
 
-module.exports = {teacherlogin,studentData}
+const studentSuggestion = async(req,res)=>{
+    const {name, id,suggestion}=req.body
+    try{
+        const userexist = await Suggestion.findOne({username:name});
+        console.log("existing user",userexist)
+        if(userexist)
+        {
+            await userexist.suggestion.push(suggestion);
+            await userexist.save();
+            res.status(200).json({message:"updated the latest suggestion"})
+        }
+        else{
+            const create = await Suggestion.create({
+                username:name,
+                id:id,
+                suggestion:[suggestion]
+            })
+            res.status(200).json({message:"suggestion created succesfully in backend"})
+        }
+    }
+    catch(error)
+    {
+        res.status(400).json({message:error});
+    }
+}
+
+const getSuggestion = async(req,res)=>{
+    const suggestions = await Suggestion.find({})
+    res.status(200).json(suggestions)
+}
+
+module.exports = {teacherlogin,studentData,studentSuggestion,getSuggestion}
